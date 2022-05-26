3929aa60b231c5ed88fb0b041a95169aefcd1339
Megan Fuhr <MeganFuhr@gmail.com>
2022-05-26 15:18:19 -0400

    .div__search {     width: 300px !important;     margin-left: 11rem !important;   } worked. but it doesn't feel right.

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Search() {
   return (
     <div className="div__search">
-      <h3 className="p__search_text">Bird Watching in New England</h3>
+      <h3 className="h3__search_text">Bird Watching in New England</h3>
       <input
         className="input__search"
         type="text"
