350cabbaeac6b88520bcd52ddc2b8fb6272279c3
Megan Fuhr <MeganFuhr@gmail.com>
2022-05-27 07:58:53 -0400

    updated image url to include w&h, increased quality to 50

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Search() {
   return (
     <div className="div__search">
-      <h3 className="h3__search_text">Bird Watching in New England</h3>
+      <h1 className="h1__search_text">Bird Watching in New England</h1>
       <input
         className="input__search"
         type="text"
