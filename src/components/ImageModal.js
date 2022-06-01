7f4ffe09ff6d5fa752f86c3b46b39dd877f44762
Megan Fuhr <MeganFuhr@gmail.com>
2022-06-01 14:13:56 -0400

    added styling and modal fine tuning to include onLoad using state

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 
-export default function ImageModal({ open, onClose, imgUrl, description }) {
-  if (!open) return null;
+export default function ImageModal({
+  open,
+  onClose,
+  imgUrl,
+  name,
+  description,
+}) {
+  const [loaded, setLoaded] = useState(false);
 
+  if (!open) return null;
   return ReactDom.createPortal(
     <div className="modal__overlay">
-      <div className="div_image-modal">
-        <button onClick={onClose}>Close</button>
-        <img src={`${imgUrl}`} alt={description} />
+      <div className="div__image-modal">
+        <img
+          src={`${imgUrl}`}
+          alt={description}
+          onLoad={() => setLoaded(true)}
+        />
+        {!loaded && "Loading . . ."}
+        <div className="div__image-modal-button-container">
+          {name}
+          <button className="button__image-modal" onClick={onClose}>
+            Close
+          </button>
+        </div>
       </div>
     </div>,
     document.getElementById("portal")
