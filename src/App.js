d8a904548f0a003a8be5b1dd89cc51b33b8e7ea3
Megan Fuhr <MeganFuhr@gmail.com>
2022-06-20 07:59:58 -0400

    working for first 4.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import Title from "./components/Title";
 import Card from "./components/Card";
 import Search from "./components/Search";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import axios from "axios";
 import Loading from "./components/Loading";
 import UseBirdSearch from "./UseBirdSearch";
 
 function App() {
-  const { birds, isLoading } = UseBirdSearch(`query {
-  birdCollection(skip:0 limit:3) {
+  const [skipNum, setSkipNum] = useState(0);
+  const limitNum = 4;
+  const query = `query {
+  birdCollection(skip:${skipNum} limit:${limitNum}) {
 	total
     items {
       sys {
@@ -28,13 +30,26 @@ function App() {
       }
     }
   }
-}`);
+}`;
+
+  const lastCard = () => {
+    console.log("in lastCard");
+    // setSkipNum((prev) => prev + limitNum);
+    // UseBirdSearch(query);
+  };
+
+  const { birds, isLoading } = UseBirdSearch(query);
 
   const DisplayBirdContent = () => {
     return (
       <>
-        {birds[0].items.map((item) => {
-          return <Card key={item.sys.id} {...item} />;
+        {birds[0].items.map((item, index) => {
+          console.log(item);
+          if (birds[0].items.length === index - 1) {
+            lastCard();
+          } else {
+            return <Card key={item.sys.id} {...item} />;
+          }
         })}
       </>
     );
