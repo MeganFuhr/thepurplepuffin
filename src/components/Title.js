340b3bce7002766d501495103b01a9de71ae6531
Megan Fuhr <MeganFuhr@gmail.com>
2022-07-07 07:21:41 -0400

    Implmented darkmode for title span to a light purple.

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,4 @@
 import React from "react";
-import puffin from "../img/puffin.png";
 import puffinsm from "../img/puffin-small.png";
 
 export default function Title(darkMode) {
@@ -11,7 +10,15 @@ export default function Title(darkMode) {
         }`}
       >
         <h1 className="h1__title">
-          The <span className="h1__span">Purple</span> Puffin
+          The
+          <span
+            className={`${
+              darkMode.darkMode ? "darkmode-h1__span" : "lightmode-h1__span"
+            }`}
+          >
+            Purple
+          </span>
+          Puffin
         </h1>
         <img className="img__title" src={puffinsm} alt="Cartoon puffin" />
       </div>
